ad07d119721297536e8db962a120f5dd165e864c
Sacha Gotainer <sgotainer@hotmail.com>
2022-08-17 18:36:40 +0200

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  Flat.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: rand(50..300),
+    number_of_guests: rand(1..10)
+  )
+end
